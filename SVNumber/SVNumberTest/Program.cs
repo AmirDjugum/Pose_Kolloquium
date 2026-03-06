92cbe4a83cd9620aaea9872a25c6af8c5ba25283
AmirDjugum <106150352+AmirDjugum@users.noreply.github.com>
2026-03-06 16:50:01 +0100

    SVNummer

---
+++ SVNumber/SVNumberTest/Program.cs
@@ -0,0 +1,36 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 1xHIF
+ *--------------------------------------------------------------
+ *              Musterlösung-HA
+ *--------------------------------------------------------------
+ * Description: Test a SV Number
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+
+namespace SVNummerTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Check a SV Number: ");
+        Console.WriteLine("**********************");
+
+        string svNumber;
+        do
+        {
+            Console.Write("Please enter a SV Number: ");
+            svNumber = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(svNumber))
+            {
+                var result = SVNummer.IsSvNumberValid(svNumber) ? "valid" : "invalid";
+                Console.WriteLine($"The SV Number {svNumber} is {result}.");
+            }
+        } while (!string.IsNullOrEmpty(svNumber));
+
+    }
+}
---
+++ SVNumber/SVNumberTest/SVNummer.cs
@@ -0,0 +1,50 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 6ABIF
+ *--------------------------------------------------------------
+ *              HA
+ *--------------------------------------------------------------
+ * Description: Test a SV Number
+ *--------------------------------------------------------------
+ */
+
+namespace SVNummerTest
+{
+    public class SVNummer
+    {
+        static bool OnlyContainsDigits(string svNumber)
+        {
+            foreach (char c in svNumber)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static int CharToDigit(char c)
+        {
+            return c - '0';
+        }
+
+        public static bool IsSvNumberValid(string svNumber)
+        {
+            int[] weight = { 3, 7, 9, 0, 5, 8, 4, 2, 1, 6 };
+
+            bool isSvOk = svNumber.Length == 10 && OnlyContainsDigits(svNumber);
+
+            if (isSvOk)
+            {
+                int sum = 0;
+                for (int i = 0; i < svNumber.Length; i++)
+                {
+                    sum += weight[i] * CharToDigit(svNumber[i]);
+                }
+
+                isSvOk = CharToDigit(svNumber[3]) == sum % 11;
+            }
+            return isSvOk;
+        }
+    }
+}
